90877c586ebce27fe2d0d1b5b43ce2e7e6b99456
Cranival <111667804+Cranival@users.noreply.github.com>
2022-10-01 13:53:11 +0700

    Update main.js

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ global.loadDatabase = async function loadDatabase() {
 }
 loadDatabase()
 
-global.authFile = `${opts._[0] || 'session'}.data.json`
+global.authFile = `${opts._[0] || 'kannabot'}.data.json`
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
 const connectionOptions = {
