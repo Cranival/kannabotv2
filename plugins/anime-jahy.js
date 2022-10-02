64585241673916e83ac05d5d05dee48dc38e203d
Cranival <111667804+Cranival@users.noreply.github.com>
2022-10-02 10:14:01 +0700

    Update anime-jahy.js

--- plugins/anime-jahy.js
+++ plugins/anime-jahy.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://api.xteam.xyz/randomimage/jahy?APIKEY=YOURAPIKEY'
+	let url = 'https://api.xteam.xyz/randomimage/jahy?APIKEY=be94e9adc6c16fe9'
 	conn.sendButton(m.chat, 'Wangy wangy (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(jahy)$/i
