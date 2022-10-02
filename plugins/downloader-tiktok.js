a43a4f0e0f99b094de0ffca5c30c2009194b245f
Cranival <111667804+Cranival@users.noreply.github.com>
2022-10-02 10:06:53 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         .catch(async _ => await tiktokdlv3(args[0]))
     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
     if (!url) throw 'Can\'t download video!'
-    conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
+    conn.sendHydrated(m.chat, `${htki} Kanna BOT⁩ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(),
         url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [
         ['ᴅᴏɴᴀꜱɪ', `.donasi`],
         ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`],
