6c21f55617206e60e93330a98fdc0f5ed308ffe9
Cranival <111667804+Cranival@users.noreply.github.com>
2022-10-03 16:10:54 +0700

    Update nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -5,10 +5,21 @@ if (db.data.chats[m.chat].nsfw == false && m.isGroup) return conn.sendButton(m.c
 	
     if (!text) throw `Use example ${usedPrefix}${command} Sagiri`
     const res = await (await googleImage('rule34 ' + text)).getRandom()
-    conn.sendButton(m.chat,` \`\`\`➩ Random Nsfw Rule34 ${text ? text.capitalize() : false}\`\`\` `, wm.date, res,['ɴᴇxᴛ', `.${command} ${text}`], m)
+    conn.sendButton(m.chat,` \`\`\`➩ Random Nsfw Rule34 ${text ? text.capitalize() : true}\`\`\` `, wm.date, res,['ɴᴇxᴛ', `.${command} ${text}`], m)
 }
 handler.help = ['rule34 <character>']
 handler.tags = ['nsfw']
 handler.command = ['rule34']
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = 1
 
 export default handler
